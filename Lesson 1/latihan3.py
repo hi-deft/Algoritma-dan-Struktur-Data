baf75c2f778af546d79157a53512c999fca108da
qaidhaidaradila <qaidhaidaradila@gmail.com>
2025-05-11 16:10:41 +0700

    add latihan3.py with greeting and squaring functions

---
+++ Lesson 1/latihan3.py
@@ -0,0 +1,11 @@
+def ucapkansalam():
+    print("Assalamu'alaikum!")
+
+def sapa(nama):
+    ucapkansalam()
+    print('Halo', nama)
+    print('Selamat belajar!')
+
+def kuadratkan(b):
+    h = b * b
+    return h
